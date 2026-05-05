3052c27278283dcf3f303b02438d4771950c2cab
Sohaib Akhtar <sohaib.akhtar@contour-software.com>
2026-05-05 18:10:05 +0500

    Completed 35.Search Insert Position

--- Arrays/27.Remove Element.py
+++ Arrays/27.Remove Element.py
@@ -8,6 +8,9 @@ class Solution:
         index=0
         for i in range(len(nums)):
             if nums[i]!=val:
+                nums[index]=nums[i]
+                index+=1
+        return index
 
 
 
