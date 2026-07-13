13674f342df7b8627d739deeae1acdd051b52e3d
Sohaib-akhter04 <akhtersohaib56@gmail.com>
2026-07-13 16:32:11 +0500

    Completed problem#448 and pushed

--- Arrays/448.Find_All_Numbers_Disappeared_in_an_Array.py
+++ Arrays/448.Find_All_Numbers_Disappeared_in_an_Array.py
@@ -7,12 +7,11 @@ class Solution:
         for num in nums:
             index=abs(num)-1
             nums[index]=-abs(nums[index])
-        print(nums)
         res=[]
         for i in range(len(nums)):
             if nums[i]>0:
                 res.append(i+1)
-        print(res)
+        return res
 
 solution=Solution()
 nums = [4,3,2,7,8,2,3,1]
